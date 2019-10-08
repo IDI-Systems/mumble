c4fda646fb5f67b90dc01c8c9ca6f5b398467cea
Robert Adam <dev@robert-adam.de>
2019-10-08 16:52:22 +0200

    made sure Global.h is the last include

--- src/mumble/ClientUser.cpp
+++ src/mumble/ClientUser.cpp
@@ -6,9 +6,11 @@
 #include "ClientUser.h"
 
 #include "Channel.h"
-#include "Global.h"
 #include "AudioOutput.h"
 #include "PluginManager.h"
+
+// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name (like protobuf 3.7 does). As such, for now, we have to make this our last include.
+#include "Global.h"
 
 QHash<unsigned int, ClientUser *> ClientUser::c_qmUsers;
 QReadWriteLock ClientUser::c_qrwlUsers;
